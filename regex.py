696dd45754d7ec86fc277a11a774a3c79f14bbb7
Sebastián Acosta <sebitas_sanguicho@hotmail.com>
2025-03-24 21:48:34 -0600

    programa regex en python

---
+++ regex.py
@@ -0,0 +1,14 @@
+import re
+
+# Definir la expresión regular
+regex = r"^b(a(r(ad)?|lrog)|and|eleg)"
+
+while True:
+    palabra = input("Ingresa una palabra (o 'salir' para terminar): ")
+    if palabra.lower() == 'salir':
+        break
+    
+    if re.search(regex, palabra):
+        print(f"{palabra} -> Coincide")
+    else:
+        print(f"{palabra} -> No coincide")
